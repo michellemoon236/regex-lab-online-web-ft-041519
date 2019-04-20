e90636b68cfa2360f2825d2db6de957f92386f5d
Michelle Moon <michellemoon236@gmail.com>
2019-04-20 18:15:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,7 +10,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(//)
 end
 
 def words_five_letters_long(text)
@@ -18,7 +18,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/^[A-Z]/)
 end
 
 def valid_phone_number?(phone)
