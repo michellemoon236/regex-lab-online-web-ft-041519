3ad279a0d8c99381c39d86eabb1c718ac75fc7c6
Michelle Moon <michellemoon236@gmail.com>
2019-04-20 18:19:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,7 +18,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/^[A-Z].+[.?]$/)
+  if text.scan(/^[A-Z].+[.?]$/).empty?
+    false
+  else
+    true
 end
 
 def valid_phone_number?(phone)
