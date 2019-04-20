8e3af4027e0bc108189f9776d97037f04b4eb475
Michelle Moon <michellemoon236@gmail.com>
2019-04-20 18:42:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,7 +26,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- if text.match(/\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/).empty?
+ if text.match(/d{10}/).empty?
     false
   else
     true
