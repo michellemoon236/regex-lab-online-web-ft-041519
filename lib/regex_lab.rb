218b09496234c9e3f9e02f5e181e49a92d26cb9e
Michelle Moon <michellemoon236@gmail.com>
2019-04-20 18:35:54 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,5 +26,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- if text.
+ if text.scan(/\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/).empty?
+    false
+  else
+    true
+  end
 end
